0ed8dfb334d016815a103261a454b85d3c07d534
alewnardu <lewnardu@gmail.com>
2025-02-08 15:50:08 -0300

    Altera configuração de variaveis

--- app/config.py
+++ app/config.py
@@ -3,10 +3,10 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'minha_chave_secreta')
     WTF_CSRF_ENABLED = True
-    USERNAME = os.getenv('DB_USERNAME', 'admin')
-    PASSWORD = os.getenv('DB_PASSWORD', 'password')
-    HOST = os.getenv('DB_HOST', 'default.mysql.pythonanywhere-services.com')
-    DATABASE = os.getenv('DB_NAME', 'papadobradofc$default')
+    USERNAME = os.getenv('DB_USERNAME', 'papadobradofc')
+    PASSWORD = os.getenv('DB_PASSWORD', 'Senh4ppfc')
+    HOST = os.getenv('DB_HOST', 'papadobradofc.mysql.pythonanywhere-services.com')
+    DATABASE = os.getenv('DB_NAME', 'papadobradofc$papadobradofc')
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}?charset=utf8mb4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOGIN_VIEW = 'auth.login'
